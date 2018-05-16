11d7c77416564c689ff138d2e1a8a2f83567ded6
kpraneethGupta <38503153+kpraneethGupta@users.noreply.github.com>
2018-05-16 16:18:50 +0530

    my favourite sorting algorithm

---
+++ sorting.cpp
@@ -0,0 +1,15 @@
+ algorithm for bubble sort
+step 1: start
+step 2: input i,j
+step 3:read a[n]
+Step 4: Repeat Steps 2 and 3 for i=1 to 10
+Step 5: Set j=1
+Step 6: Repeat while j<=n
+          (A) if  a[i] < a[j]
+             Then interchange a[i] and a[j]
+             [End of if]
+         (B) Set j = j+1
+        [End of Inner Loop]
+    [End of Step 1 Outer Loop]
+step 7: print a[n]
+Step 8: stop
